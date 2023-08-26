844be5902d404d812a7345cfb5dad34ef3ea2f8c
MrQuack5129 <tmaxeysmith@gmail.com>
2023-08-26 14:22:15 +0100

    Player Controller

--- colab/Assets/PlayerController.cs
+++ colab/Assets/PlayerController.cs
@@ -4,23 +4,5 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public Rigidbody rb;
-    public GameObject cam;
-    public float sensitivity;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        Cursor.lockState = CursorLockMode.Locked;
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        float mouseX = Input.GetAxis("Mouse X");
-        float mouseY = Input.GetAxis("Mouse Y");
-
-        transform.Rotate(Vector3.up * mouseX * sensitivity);
-        cam.transform.Rotate(-Vector3.right * mouseY * sensitivity);
-    }
+    
 }
