77e79c0d32f1a365c43838bdbdc10e5000ed30f4
MrQuack5129 <tmaxeysmith@gmail.com>
2023-08-29 10:57:00 +0100

    Drone Stuff

--- colab/Assets/DroneEnemy.cs
+++ colab/Assets/DroneEnemy.cs
@@ -4,16 +4,45 @@ using UnityEngine;
 
 public class DroneEnemy : MonoBehaviour
 {
+    [Header("References")]
     public Rigidbody rb;
+    public Transform targetObject;
+
+    [Header("Movement Settings")]
     public float speed;
     public float flyHeight;
+    public float flyHeightRandomOffset;
     public float upForce;
+    public float torqueForce;
+    public float maxRollAngle = 15.0f;
+    [Range(0f, 1f)]
+    public float centerSpeed;
+    public float maxSpeed;
+    public bool shouldMove;
+    public float timeBetweenMoves;
+    public Vector3 randomPos;
+    public float maxAnglularVelocity;
+
+    [Header("Shoot Settings")]
+    public float fireRate;
+    public GameObject projectile;
+    public Transform gunPoint;
+    private bool canShoot;
+    public float shootAngle;
+    public float shootForce;
+    public float bulletsToShootAtOnce;
+
+    [Header("Move Settings")]
+    public float maxRangeMove;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
-        
+        canShoot = true;
+        shouldMove = true;
+        randomPos = transform.position + new Vector3(Random.Range(-maxRangeMove, maxRangeMove), 0f, Random.Range(-maxRangeMove, maxRangeMove));
+        flyHeight = flyHeight + Random.Range(-flyHeightRandomOffset, flyHeightRandomOffset);
     }
 
     // Update is called once per frame
@@ -28,5 +57,86 @@ public class DroneEnemy : MonoBehaviour
         {
             rb.AddForce(Vector3.up * upForce, ForceMode.Force);
         }
+
+        if(targetObject != null)
+        {
+            Rotate();
+        }
+
+        Vector3 directionToTarget = targetObject.position - transform.position;
+        float angle = Vector3.Angle(directionToTarget, transform.forward);
+        angle = Mathf.Abs(angle);
+
+        if (angle <= shootAngle && canShoot)
+        {
+            Shoot();
+        }
+
+        if (shouldMove)
+        {
+            Move();
+        }
+
+        if(rb.angularVelocity.magnitude > maxAnglularVelocity)
+        {
+            rb.angularVelocity = rb.angularVelocity.normalized * maxAnglularVelocity;
+        }
+    }
+
+    void Shoot()
+    {
+        canShoot = false;
+        Invoke(nameof(ResetShoot), 1f / fireRate);
+        float bulletsShot = 0;
+        while (bulletsShot < bulletsToShootAtOnce)
+        {
+            GameObject bullet = Instantiate(projectile, gunPoint.transform.position, Quaternion.Euler(gunPoint.transform.forward));
+            Rigidbody rb = bullet.GetComponent<Rigidbody>();
+            Vector3 direction = gunPoint.transform.forward;
+            rb.AddForce(direction * shootForce, ForceMode.Impulse);
+            //Instantiate(shootSound, gunPoint);
+            bulletsShot++;
+        }
+    }
+
+    void ResetShoot()
+    {
+        canShoot = true;
+    }
+
+    void Rotate()
+    {
+        Vector3 directionToTarget = targetObject.position - transform.position;
+        float angle = Vector3.Angle(directionToTarget, transform.forward);
+        angle = Mathf.Abs(angle);
+
+        // Calculate the rotation axis (cross product between forward and target direction).
+        Vector3 rotationAxis = Vector3.Cross(transform.forward, directionToTarget);
+
+        // Apply torque to the Rigidbody to make it face the target.
+        rb.AddTorque(rotationAxis * torqueForce * angle, ForceMode.Force);
+
+        transform.rotation = Quaternion.Euler(new Vector3(transform.rotation.eulerAngles.x, transform.rotation.eulerAngles.y, 0f));
+
+        if(angle < 0.5f && rb.angularVelocity.magnitude > 0f )
+        {
+            rb.angularVelocity = Vector3.Lerp(rb.angularVelocity, Vector3.zero, centerSpeed);
+        }
+    }
+
+    void Move()
+    {
+        Vector3 direction = randomPos - transform.position;
+        float distance = Vector3.Distance(new Vector3(randomPos.x, transform.position.y, randomPos.z), transform.position);
+        if (rb.velocity.magnitude < maxSpeed)
+        {
+            rb.AddForce(direction * speed, ForceMode.Force);
+        }
+        if (distance < 1f)
+        {
+            randomPos = transform.position + new Vector3(Random.Range(-maxRangeMove, maxRangeMove), 0f, Random.Range(-maxRangeMove, maxRangeMove));
+        }
+
+        Debug.Log(distance);
     }
 }
--- colab/Assets/Scripts/Projectile.cs
+++ colab/Assets/Scripts/Projectile.cs
@@ -6,13 +6,15 @@ public class Projectile : MonoBehaviour
 {
     public GameObject destroyEffect;
     public Rigidbody rb;
+    public float maxLifeSpan = 30f;
 
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.tag != "Projectile")
         {
             Debug.Log("Bullet Hit || Normal: " + collision.contacts[collision.contacts.Length - 1].normal);
-            Instantiate(destroyEffect, collision.contacts[collision.contacts.Length - 1].point, Quaternion.LookRotation(collision.contacts[collision.contacts.Length - 1].normal));
+            GameObject de = Instantiate(destroyEffect, collision.contacts[collision.contacts.Length - 1].point, Quaternion.LookRotation(collision.contacts[collision.contacts.Length - 1].normal));
+            Destroy(de, 5f);
             if (collision.transform.GetComponent<Rigidbody>() != null) 
             {
                 Rigidbody hitRb = collision.transform.GetComponent<Rigidbody>();
@@ -24,5 +26,15 @@ public class Projectile : MonoBehaviour
             }
             Destroy(gameObject);
         }
+    }
+
+    private void Start()
+    {
+        Invoke(nameof(Destroy), maxLifeSpan);
+    }
+
+    private void Destroy()
+    {
+        Destroy(gameObject);
     }
 }
